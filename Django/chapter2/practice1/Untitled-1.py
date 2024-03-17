3d17e93d517eec46867ed3a8a814ac617bedd397
AmirHSoukhteh <ascr.amir@gmail.com>
2024-03-17 22:32:24 +0330

    practice three

---
+++ Django/chapter2/practice1/Untitled-1.py
@@ -0,0 +1,20 @@
+
+def count_everything(string):
+    l = [c for c in string]
+    char_count = 0
+    digit_count = 0
+
+    for i in range(l.count(" ")):
+        l.remove(" ")
+
+    for c in l:
+        if c.isdigit():
+            digit_count += 1
+        else:
+            char_count += 1
+    
+    return {"charcaters":char_count, "numbers":digit_count}
+
+result = count_everything(input("enter your text: "))
+print(result)
+    
